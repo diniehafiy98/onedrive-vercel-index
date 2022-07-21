77a4888af0a78857bea45a27e92cc110e164f3d1
dinie <105410968+diniehafiy98@users.noreply.github.com>
2022-07-21 09:07:05 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/IPTV MDH'],
+  protectedRoutes: ['/IPTV MDH', '/TRADING'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:mdhsuccessful@gmail.com',
